b896787eca8e5a2020894fee61df0fe9a2872779
EC2 Default User <ec2-user@ip-172-31-60-57.ec2.internal>
2026-03-16 17:09:08 +0000

    Fixing EB 3

--- venue_project/settings.py
+++ venue_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.vfs.cloud9.us-east-1.amazonaws.com"
+]
